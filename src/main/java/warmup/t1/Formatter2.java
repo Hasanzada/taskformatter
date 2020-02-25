489059e438eb1a93a12e58b27b5bcb955889d2e3
Zulfuqar Asgerzade <asgerzade.zulfuqar@gmail.com>
2020-02-25 10:08:09 +0400

    add task1

--- src/main/java/warmup/t1/Formatter2.java
+++ src/main/java/warmup/t1/Formatter2.java
@@ -2,10 +2,8 @@ package warmup.t1;
 
 
 public class Formatter2 extends Formatter0 {
-     String s;
 
      void print(String a, Formatter0 f) {
-        s = a.toUpperCase();
-        System.out.println(s);
+        System.out.println(a.toUpperCase());
     }
 }
--- src/main/java/warmup/t1/Formatter3.java
+++ src/main/java/warmup/t1/Formatter3.java
@@ -2,9 +2,6 @@ package warmup.t1;
 
 public class Formatter3 extends Formatter0 {
 
-    String s;
-
-
      void print(String a, Formatter0 f) {
          System.out.println("************");
          System.out.println("*   " + a + "   *");
